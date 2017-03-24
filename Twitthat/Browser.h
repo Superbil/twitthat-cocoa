e95a1fc368cb5539934e272e5b24a787f184db64
Superbil <superbil@gmail.com>
2017-03-24 22:14:41 +0800

    New class Browser

---
+++ Twitthat/Browser.h
@@ -0,0 +1,13 @@
+//
+//  Browser.h
+//  Twitthat
+//
+//  Created by Superbil on 2017/3/24.
+//  Copyright © 2017年 Superbil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Browser : NSObject
+
+@end
---
+++ Twitthat/Browser.m
@@ -0,0 +1,13 @@
+//
+//  Browser.m
+//  Twitthat
+//
+//  Created by Superbil on 2017/3/24.
+//  Copyright © 2017年 Superbil. All rights reserved.
+//
+
+#import "Browser.h"
+
+@implementation Browser
+
+@end
