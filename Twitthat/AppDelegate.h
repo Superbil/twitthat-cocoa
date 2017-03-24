c23dca53eb9a4100c4c8d1ef6f36435c8de1e984
Superbil <superbil@gmail.com>
2017-03-24 22:00:38 +0800

    Init twitthat project

---
+++ Twitthat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitthat
+//
+//  Created by Superbil on 2017/3/24.
+//  Copyright © 2017年 Superbil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Twitthat/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Twitthat
+//
+//  Created by Superbil on 2017/3/24.
+//  Copyright © 2017年 Superbil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
