35c41e1008fedb4a6146280b2edfc6978ef60bde
Superbil <superbil@gmail.com>
2017-03-25 04:15:51 +0800

    Impelemented Link

--- Twitthat/Link.m
+++ Twitthat/Link.m
@@ -17,9 +17,25 @@
 
 - (instancetype)initWithString:(NSString *)aString {
     if (self = [super init]) {
-
+        if (aString) {
+            NSString *split = @"::superbil::";
+            NSRange r = [aString rangeOfString:split];
+            if (r.location != NSNotFound) {
+                NSString *link = [aString substringWithRange:NSMakeRange(0, r.location)];
+                self.url = [NSURL URLWithString:link];
+                self.title = [aString substringWithRange:NSMakeRange(r.location + split.length, aString.length - (r.location + split.length))];
+            }
+        }
     }
     return self;
+}
+
+- (NSString *)description {
+    NSMutableString *d = [[NSMutableString alloc] init];
+    [d appendFormat:@"<%@>\n", NSStringFromClass([self class])];
+    [d appendFormat:@"  title: %@\n", self.title];
+    [d appendFormat:@"  url: %@", self.url];
+    return d;
 }
 
 @end
