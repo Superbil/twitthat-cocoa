26f1e81f1004d8d42289a865b1f1764f3e7912fc
Superbil <superbil@gmail.com>
2017-03-31 15:55:07 +0800

    Basic status menu item

--- Twitthat/AppDelegate.m
+++ Twitthat/AppDelegate.m
@@ -8,13 +8,25 @@
 
 #import "AppDelegate.h"
 
+#import <Social/Social.h>
+
 #import "Browser.h"
+#import "Link.h"
+
+typedef NS_ENUM(NSUInteger, BrowserSource) {
+    BrowserSourceChrome = 1,
+    BrowserSourceFirefox,
+    BrowserSourceSafari,
+};
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSMenu *statusMenu;
 @property (strong) NSStatusItem *statusBar;
+@property (strong) NSArray<NSString *> *browserTitles;
+@property (strong) NSArray<NSString *> *actionTitles;
+@property (assign) BrowserSource currentSource;
 
 @end
 
@@ -24,8 +36,51 @@
     self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     self.statusBar.title = @"TT";
     self.statusBar.menu = self.statusMenu;
+
+    self.browserTitles = @[NSLocalizedString(@"{{SM_Get_Chrome}}", nil),
+                           NSLocalizedString(@"{{SM_Get_Firefox}}", nil),
+                           NSLocalizedString(@"{{SM_Get_Safari}}", nil)];
+
+    self.actionTitles = @[@"Reading:",
+                          @"Looking at:",
+                          @"Watching:",
+                          @"Listening to:",
+                          @"Laughing at:",
+                          @"Curious about:",
+                          @"Crying for:",
+                          @"Orz at:",
+                          @"Disappointed at:",
+                          @"Responding to:",
+                          @"Waiting for:",
+                          @"Looking forward to:",
+                          @"→_→",
+                          @"@",
+                          ];
+
+    [self initMenu];
 }
 
+- (void)initMenu {
+    NSInteger s = [[NSUserDefaults standardUserDefaults] integerForKey:@"Current Browser"];
+    NSMenu *browserMenu = [[NSMenu alloc] initWithTitle:@"Browser"];
+    [self.browserTitles enumerateObjectsUsingBlock:^(NSString * _Nonnull t, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:t
+                                                      action:@selector(browserChange:)
+                                               keyEquivalent:@""];
+        [browserMenu addItem:item];
+        if (s > 0 && idx == (s - 1)) {
+            // show checkmark
+        }
+    }];
+    NSMenuItem *p = [self.statusMenu itemAtIndex:1];
+    p.submenu = browserMenu;
+
+    NSUInteger insertIndex = 0;
+    [self.actionTitles enumerateObjectsUsingBlock:^(NSString * _Nonnull t, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:t action:@selector(itemAction:) keyEquivalent:@""];
+        [self.statusMenu insertItem:item atIndex:insertIndex + idx];
+    }];
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
@@ -33,16 +88,41 @@
 
 #pragma mark - Actions
 
-- (IBAction)chromeAction:(id)sender {
-    NSLog(@"%@", [[Browser get] chrome]);
+- (IBAction)browserChange:(id)sender {
+    NSMenuItem *menuItem = sender;
+    if ([menuItem.title isEqualToString:NSLocalizedString(@"{{SM_Get_Chrome}}", nil)]) {
+        self.currentSource = BrowserSourceChrome;
+    } else if ([menuItem.title isEqualToString:NSLocalizedString(@"{{SM_Get_Firefox}}", nil)]) {
+        self.currentSource = BrowserSourceFirefox;
+    } else if ([menuItem.title isEqualToString:NSLocalizedString(@"{{SM_Get_Safari}}", nil)]) {
+        self.currentSource = BrowserSourceSafari;
+    }
+    [[NSUserDefaults standardUserDefaults] setInteger:self.currentSource forKey:@"Current Browser"];
 }
 
-- (IBAction)firefoxAction:(id)sender {
-    NSLog(@"%@", [[Browser get] firefox]);
-}
+- (IBAction)itemAction:(id)sender {
+    NSMenuItem *menuItem = sender;
 
-- (IBAction)safariAction:(id)sender {
-    NSLog(@"%@", [[Browser get] safari]);
+    Link *l = nil;
+    switch (self.currentSource) {
+        case BrowserSourceChrome:
+            l = [[Browser get] chrome];
+            break;
+        case BrowserSourceFirefox:
+            l = [[Browser get] firefox];
+            break;
+
+        case BrowserSourceSafari:
+            l =  [[Browser get] safari];
+            break;
+
+        default:
+            break;
+    }
+
+    if (l) {
+        NSLog(@"%@ %@ %@", menuItem.title, l.title, l.url);
+    }
 }
 
 @end
